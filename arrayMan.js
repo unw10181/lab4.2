06b80d1be70f2b02c4d074225e72b581b1fafa25
unw10181 <uchennanwilliams@gmail.com>
2025-11-07 13:53:01 -0500

    task2

--- arrayMan.js
+++ arrayMan.js
@@ -1,16 +1,26 @@
 let shoppingList = [];
 
-const addItem =  (param, arr) => {
-    arr.push(param)
-}
+const addItem = (param, arr) => {
+  if (arr.includes(param)) {
+    return "Item already in storage";
+  } else {
+    arr.push(param);
+  }
+};
 
 const removeLastItem = (arr) => {
-    arr.pop();
-}
+  arr.pop();
+};
 
 const displayList = (arr) => {
-    console.log(arr)
-}
+  console.log(arr);
+};
+
+const filterItems = (param, arr) => {
+  let items;
+  items = arr.filter((item) => item.includes(param));
+  return items;
+};
 
 addItem("twix", shoppingList);
 displayList(shoppingList);
@@ -18,11 +28,14 @@ addItem("snickers", shoppingList);
 addItem("skittles", shoppingList);
 addItem("mnms", shoppingList);
 displayList(shoppingList);
-removeLastItem(shoppingList)
-displayList(shoppingList)
-
-
+removeLastItem(shoppingList);
+displayList(shoppingList);
+console.log(filterItems("s", shoppingList));
 
 // How did array methods like push() and filter() help you manipulate data in this activity?
+// Was able to add and retract things from array
 // What challenges did you face when trying to filter and search for items in the array?
+//Web3 documentation on using filter was not good. But MDN showed a clear example on how to utilize the function.
+// it is a bit tricky though.
 // How would you modify this code to make it more efficient or user-friendly?
+// Maybe refactor in a way that there is less repetition within my code.
